58f0c97ff82a36ffc993fc0f00092f6338f94851
C3f3 <armandoher01@gmail.com>
2020-08-04 04:29:02 -0300

    slider modificado,creando secciones del inicio

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
         <!------------------ slider------------->
         <div class="section" id="home">
             <header class="main-header aos-item" data-aos="fade-up">
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
                     <ol class="carousel-indicators">
                         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -12,26 +12,37 @@
                     </ol>
                     <div class="carousel-inner" role="listbox">
                         <!-- Slide One - Set the background image for this slide in the line below -->
-                        
-                        <div class="carousel-item active" style="background-image: url('/img/slider0.jpg')">
-                            <div class="carousel-caption d-md-block">
-                                <h3 href="#" class="display-4">Seriedad</h3>
-                                <p class="lead">Cumplimos con todas las normativas vigentes.</p>
-                            </div>
+                        <div class="carousel-item active" data-interval="2000">
+                            <a href="">
+                                <img src="/img/slider0.jpg" alt="">
+                                <div class="carousel-caption d-md-block">
+                                    <h3 href="#" class="display-4">Seriedad</h3>
+                                    <p class="lead">Cumplimos con todas las normativas vigentes.</p>
+                                </div>
+                            </a>
+                            
                         </div>
                         <!-- Slide Two - Set the background image for this slide in the line below -->
-                        <div class="carousel-item" style="background-image: url('/img/slider1.jpg')">
-                            <a class="carousel-caption d-md-block">
-                                <h3 class="display-4">Excelencia</h3>
-                                <p class="lead">Aseguramos calidad en todos nuestros trabajos.</p>
+                        <div class="carousel-item" data-interval="2000">
+                            <a href="">
+                                <img src="/img/slider1.jpg" alt="" >
+                                <div class="carousel-caption d-md-block">
+                                    <h3 class="display-4">Excelencia</h3>
+                                    <p class="lead">Aseguramos calidad en todos nuestros trabajos.</p>
+                                </div>
                             </a>
+                            
                         </div>
                         <!-- Slide Three - Set the background image for this slide in the line below -->
-                        <div class="carousel-item" style="background-image: url('/img/slider2.jpg')">
-                            <div class="carousel-caption d-md-block">
-                                <h3 class="display-4">Compromiso</h3>
-                                <p class="lead">Priorizamos las necesidades de nuestros clientes.</p>
-                            </div>
+                        <div class="carousel-item" data-interval="2000">
+                            <a href="">
+                                <img src="/img/slider2.jpg" alt="">
+                                <div class="carousel-caption d-md-block">
+                                    <h3 class="display-4">Compromiso</h3>
+                                    <p class="lead">Priorizamos las necesidades de nuestros clientes.</p>
+                                </div>
+                            </a>
+                            
                         </div>
                     </div>
                     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -57,6 +68,56 @@
                     personas particulares. Acercate y te asesoramos....
                 </p>
                 <a href="about.html" class="mt-4 btn btn-success">Conocenos!</a>
+            </div>
+        </div>
+    </div>
+    <div class="section hero-image">
+        <div class="text-center uno d-flex align-items-center">
+            <div class="content mx-auto ">
+                <h1>Titulo</h1>
+                <p>Descripcion</p>
+            </div>
+            
+        </div>   
+        <div class="bg-dark py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 text-center">
+                        <h1>gaming</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa accusantium hic, 
+                            sit unde deleniti quidem rem at laudantium distinctio qui necessitatibus.
+                             Fuga ducimus dolor neque nihil ab eveniet recusandae illo!</p>
+                        <a href="" class="btn btn-success">Ver Mas</a>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="./img/secciones/gamer.jpg" alt="" class="image-card">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section hero-image">
+        <div class="text-center uno d-flex align-items-center">
+            <div class="content mx-auto ">
+                <h1>Titulo</h1>
+                <p>Descripcion</p>
+            </div>
+            
+        </div>   
+        <div class="bg-dark py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 text-center">
+                        <h1>Microelectronica</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa accusantium hic, 
+                            sit unde deleniti quidem rem at laudantium distinctio qui necessitatibus.
+                             Fuga ducimus dolor neque nihil ab eveniet recusandae illo!</p>
+                        <a href="" class="btn btn-success">Ver Mas</a>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="./img/secciones/gamer.jpg" alt="" class="image-card">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
