e03dea81cdc657b87a7aa21d503488bf62b9948e
C3f3 <armandoher01@gmail.com>
2020-08-08 02:15:01 -0300

    descarga de archivos optimizada

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" href="/css/aos.css"/>
 
     <link rel="stylesheet" href="/css/estilos.css">
     <link rel="shortcut icon" href="/img/logo-redondo.png" type="image/x-icon">
@@ -99,7 +99,7 @@
             integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
             crossorigin="anonymous"></script>
         <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
-        <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+        <script src="/js/aos.js"></script>
         <script>
           AOS.init();
         </script>
