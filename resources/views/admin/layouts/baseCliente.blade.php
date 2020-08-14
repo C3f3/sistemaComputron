92f2a087fb559669f0a3d4e830e8a59a0dbbd8d9
C3f3 <armandoher01@gmail.com>
2020-08-13 23:20:41 -0300

    panel de cliente armado

--- resources/views/admin/layouts/baseCliente.blade.php
+++ resources/views/admin/layouts/baseCliente.blade.php
@@ -77,36 +77,74 @@
                             <!--secciones-->
                             <div class="sb-sidenav-menu-heading">Acciones</div>
 
+                                <!--seccion Mi Cuenta-->
+                                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseCount" 
+                                aria-expanded="false" aria-controls="collapseLayouts">
 
+                                    <!-- cabecera Mi Cuenta-->
+                                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                    Mi Cuenta
+                                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                                </a>
+                                <div class="collapse" id="collapseCount" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                            
+                                    <!--submenu para los datos personales-->
+                                    <nav class="sb-sidenav-menu-nested nav">
+
+                                        <a class="nav-link" href="{{route('verMisDatos')}}">Mis Datos</a>
+
+                                    </nav>
+                                </div>
+
+                                <!--seccion Puntos-->
                                 <a class="nav-link collapsed" href="#" data-toggle="collapse" 
                                 data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                                     <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                                         Puntos
                                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                                 </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" 
-                            data-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="layout-static.html">Static Navigation</a>
-                                    <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
-                                </nav>
-                            </div>
-                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                Campaña
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapsePages" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="layout-static.html">Static Navigation</a>
-                                    <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
-                                </nav>
-                            </div>
+                                <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" 
+                                    data-parent="#sidenavAccordion">
+                                    <nav class="sb-sidenav-menu-nested nav">
+                                        <a class="nav-link" href="layout-static.html">Static Navigation</a>
+                                        <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
+                                    </nav>
+                                </div>
+
+                                <!--seccion Campaña-->
+                                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapseLayouts">
+                                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                    Campaña
+                                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                                </a>
+                                <div class="collapse" id="collapsePages" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                                    <nav class="sb-sidenav-menu-nested nav">
+                                        <a class="nav-link" href="layout-static.html">Static Navigation</a>
+                                        <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
+                                    </nav>
+                                </div>
+
+                                <!--seccion para trabajos-->
+                                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseWorks" 
+                                aria-expanded="false" aria-controls="collapseLayouts">
+
+                                    <!-- cabecera campaña-->
+                                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                    Trabajos
+                                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                                </a>
+                                <div class="collapse" id="collapseWorks" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                                    <!--submenu para trabajos-->
+                                    <nav class="sb-sidenav-menu-nested nav">
+                                        <a class="nav-link" href="{{route('listarTrabajos')}}">Ver Trabajos</a>
+                                        <a class="nav-link" href="">Nuevo Trabajo</a>
+                                    </nav>
+                                </div>
                         </div>
                     </div>
                     <div class="sb-sidenav-footer">
-                        <div class="small">Logged in as:</div>
-                        Start Bootstrap
+                        <div class="small">Diseñado por:</div>
+                        Computron
                     </div>
                 </nav>
             </div>
