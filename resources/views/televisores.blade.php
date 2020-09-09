8e760eb33bf618f58604ab383c12de8c158299c0
C3f3 <armandoher01@gmail.com>
2020-09-09 20:24:14 -0300

    seccion tele terminada

--- resources/views/televisores.blade.php
+++ resources/views/televisores.blade.php
@@ -68,18 +68,18 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-6">
-                    <img src="/img/secciones/reparacion/reparacion1.jpg" alt="">
+                    <img src="/img/secciones/televisores/tele1.jpg" alt="tele1">
                 </div>
                 <div class="col-md-6">
-                    <img src="/img/secciones/reparacion/reparacion2.jpg" alt="">
+                    <img src="/img/secciones/televisores/tele2.jpg" alt="tele2">
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-6">
-                    <img src="/img/secciones/reparacion/reparacion3.jpg" alt="">
+                    <img src="/img/secciones/televisores/tele3.jpg" alt="tele3">
                 </div>
                 <div class="col-md-6">
-                    <img src="/img/secciones/reparacion/reparacion4.jpg" alt="">
+                    <img src="/img/secciones/televisores/tele4.jpg" alt="tele4">
                 </div>
             </div>
             
